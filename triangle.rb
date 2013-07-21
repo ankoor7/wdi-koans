9e30da9f6e23565ef947c5c79b73c2824d1b3b97
Ankoor Patel <ankoor.p@gmail.com>
2013-07-21 23:15:02 +0100

    fin blocks

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a,b,c]
+  test_length = [a,b,c].sort!
+  raise TriangleError if (test_length[0]+test_length[1] <= test_length[2])
+  raise TriangleError if ( test_length[0] == 0 )
+  case sides.uniq.count
+  when 3
+    :scalene
+  when 2
+    :isosceles
+  when 1
+    if sides.uniq == [0]
+      raise TriangleError, 'all sides equal zero'
+    else
+      :equilateral
+    end
+  else
+    'Function does not work'
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
